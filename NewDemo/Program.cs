9bf4fe76b055a6b22cebd1c27e0a90d4803ae413
Kamini Vasantrao Suryawanshi <kaminis@cybage.com>
2022-05-30 14:51:20 +0530

    Add project files.

---
+++ NewDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
